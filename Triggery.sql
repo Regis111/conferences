8aa907aa2485121d8d10b34ce0cf40216c078340
Jerzy Wenta <jerzy.wenta1998@gmail.com>
2019-01-20 10:10:45 +0100

    dodane triggery TO DO

--- Triggery.sql
+++ Triggery.sql
@@ -10,3 +10,10 @@
 
 --Blokuje dodawanie dnia konferencji je¿eli wykracza poza czas trwania konferencji
 
+--Pilnuje, czy po zmianie iloœci zarezerwowanych miejsc na dany dzieñ zmieniona iloœæ obejmuje wszystkie zapisane osoby
+
+--Usuwanie wszystkich rezerwacji dni po usuniêciu danej rezerwacji
+
+--Usuwanie wszystkich rezerwacji warsztatów po usuniêciu danej rezerwacji dnia
+
+--Usuwanie cz³onków Warsztatu po usuniêciu rezerwacji warsztatu
